1957f9b1d0a84c50c8ebb30ea1a42f2a2e2a832c
ntlk <hi@ntlk.net>
2013-04-21 13:53:52 +0100

    let's see what we get back from twilio

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,7 +1,11 @@
 class TwilioController < ApplicationController
   def voice
+    @params = params
+    Rails.logger.debug @params
   end
 
   def sms
+    @params = params
+    Rails.logger.debug @params
   end
 end
