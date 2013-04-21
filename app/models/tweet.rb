4c5e9cc6415efcf990fddb1657505e6caf62459f
ntlk <hi@ntlk.net>
2013-04-21 16:42:21 +0100

    remove urls from tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,7 +3,12 @@ class Tweet < ActiveRecord::Base
   validates_presence_of :body, :timestamp, :uid
   validates_uniqueness_of :uid
 
+  before_create :remove_urls_from_tweet
   after_create :wake_up_subscribers
+
+  def remove_urls_from_tweet
+    self.body.sub /https?:\/\/[\S]+/, ''
+  end
 
   def wake_up_subscribers
     Subscription.make_the_calls
