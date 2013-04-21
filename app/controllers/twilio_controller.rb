abc6e74ca0b3cd83dc652a847f4b79eef2cd37d2
ntlk <hi@ntlk.net>
2013-04-21 14:31:54 +0100

    adding responses for twilio

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -2,10 +2,12 @@ class TwilioController < ApplicationController
   def voice
     @params = params
     Rails.logger.debug @params
+    render 'voice.xml.erb', :content_type => 'text/xml'
   end
 
   def sms
     @params = params
     Rails.logger.debug @params
+    render 'sms.xml.erb', :content_type => 'text/xml'
   end
 end
