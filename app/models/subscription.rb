b1b3a5c2196b028e54ef01009e071e94ff8ae1cc
ntlk <hi@ntlk.net>
2013-04-21 16:20:35 +0100

    forgotten comma

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,7 +2,7 @@ class Subscription < ActiveRecord::Base
   attr_accessible :is_active, :phone_number
   validates_presence_of :phone_number
 
-  scope :active where(:is_active => true)
+  scope :active, where(:is_active => true)
 
   def self.make_the_calls
     active.each {|subscription| subscription.execute_phone_call }
